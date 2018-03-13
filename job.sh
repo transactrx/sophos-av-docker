b56d975b99ea7c9113cc79fd6626c928abceabf7
Manuel Elaraj <melaraj@transactrx.com>
2018-03-13 10:14:36 -0400

    added option to make sophos not reset CTIME on files scanned.

--- job.sh
+++ job.sh
@@ -6,6 +6,6 @@ cd /opt/sophos-av/bin/
 while :
 do
 	./savupdate -v5
-	savscan -all --stay-on-machine -s --no-follow-symlinks --skip-special /scan -exclude /scan/cgroup /scan/sys /scan/var/run/docker /scan/home/sftproot /scan/var/lib/docker /scan/proc
+	savscan -all --stay-on-machine -s --no-follow-symlinks --skip-special --no-reset-atime /scan -exclude /scan/cgroup /scan/sys /scan/var/run/docker /scan/home/sftproot /scan/var/lib/docker /scan/proc
 	sleep $INTERVAL
 done
